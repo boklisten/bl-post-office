a9a8f09c04696fd8663b625bc57d285bf5290ac2
holskil <aholskil@gmail.com>
2019-03-21 12:29:53 +0100

    doc: added some documentation to 'test/test-environment.ts'

--- test/test-environment.ts
+++ test/test-environment.ts
@@ -4,7 +4,12 @@ import {inversifyContainer} from '../inversify.config';
 export class TestEnvironment {
   private _container: Container;
 
-  public constructor(config?: {
+  /**
+   * Constructs a new testing environment
+   *
+   * @param config - optional, can set classes to bind and classes to mock for the test environment
+   */
+  constructor(config?: {
     classesToBind?: any[];
     classesToMock?: {real: any; mock: any}[];
   }) {
@@ -20,10 +25,21 @@ export class TestEnvironment {
     }
   }
 
+  /**
+   * binds a class to the test environment
+   *
+   * @param classes - the classes to bind to the test environment
+   *
+   */
   public bind(classes: any[]) {
     classes.forEach(c => this._container.bind(c).toSelf());
   }
 
+  /**
+   * inserts a mocked class instead of the real class
+   *
+   * @param mocks - a list of real classes with their mocked class
+   */
   public mockClass(mocks: {real: any; mock?: any; mockedClass?: any}[]) {
     mocks.forEach(m => {
       if (m.mock) {
