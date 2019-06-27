e1d79520fff7303a621bece24e604da7fba2093c
holskil <aholskil@gmail.com>
2019-06-27 14:31:15 +0200

    feat: added more sms text for partly-payment, loan and rent

--- src/sms/sms-settings.ts
+++ src/sms/sms-settings.ts
@@ -10,13 +10,18 @@ export const SMS_SETTINGS = {
       'partly-payment': [
         'Hei - det nærmer seg frist for bøkene dine! Mange steder begynner å stenge før fristen 1.juli. Kun Boklistens ansatte kan ta imot bøker. Sjekk nettsiden for åpningstider og postadresse, samt informasjonsmailene vi har sendt ut. Mvh Boklisten.no',
         'Hei - det nærmer seg frist for bøkene dine! Mange steder begynner å stenge før fristen 1.juli. Kun Boklistens ansatte kan ta imot bøker. Sjekk nettsiden for åpningstider og postadresse, samt informasjonsmailene vi har sendt ut. Mvh Boklisten.no',
+        'Hei - det nærmer seg frist for bøkene dine! Mange steder begynner å stenge før fristen 1.juli. Kun Boklistens ansatte kan ta imot bøker. Sjekk nettsiden for åpningstider og postadresse, samt informasjonsmailene vi har sendt ut. Mvh Boklisten.no',
+        'Siste påminnelse! Selg bøkene dine tilbake til Boklisten.no for å slippe andre avdrag. Vi sender ut faktura på andre avdrag 1.juli. Se påminnelsesmailer for mer informasjon.',
       ],
       rent: [
         'Hei - det nærmer seg frist for bøkene dine! Mange steder begynner å stenge før fristen 1.juli. Kun Boklistens ansatte kan ta imot bøker. Sjekk nettsiden for åpningstider og postadresse, samt informasjonsmailene vi har sendt ut. Mvh Boklisten.no',
         'Hei - det nærmer seg frist for bøkene dine! Mange steder begynner å stenge før fristen 1.juli. Kun Boklistens ansatte kan ta imot bøker. Sjekk nettsiden for åpningstider og postadresse, samt informasjonsmailene vi har sendt ut. Mvh Boklisten.no',
+        'Hei - det nærmer seg frist for bøkene dine! Mange steder begynner å stenge før fristen 1.juli. Kun Boklistens ansatte kan ta imot bøker. Sjekk nettsiden for åpningstider og postadresse, samt informasjonsmailene vi har sendt ut. Mvh Boklisten.no',
+        'Siste påminnelse! Det er ikke registrert din fulle levering av bøker, og absolutt siste frist er 1.juli. Se påminnelsesmailer for mer info.',
       ],
       loan: [
         'Hei fra oss i Boklisten.no. Snart er det tid for å levere bøkene du lånte',
+        'Siste påminnelse! Det er ikke registrert din fulle levering av bøker, og absolutt siste frist er 1.juli. Se påminnelsesmailer for mer info.',
       ],
     },
   },
