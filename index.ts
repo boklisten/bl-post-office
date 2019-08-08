7f56cf1616c66edf2500332f181dc926bc609ccd
holskil <aholskil@gmail.com>
2019-08-08 11:17:56 +0200

    build: export of types from recipient interface in index.ts

--- index.ts
+++ index.ts
@@ -5,4 +5,11 @@ export const postOffice = inversifyContainer.get<PostOffice>(PostOffice);
 export {PostOffice} from './src/post-office';
 export {MessageOptions} from './src/interfaces/message-options';
 export {MessageMediums} from './src/interfaces/message-mediums';
-export {Recipient} from './src/interfaces/reciptient';
+export {
+  Recipient,
+  Delivery,
+  Payment,
+  Order,
+  EmailContentSettings,
+  ItemList,
+} from './src/interfaces/reciptient';
