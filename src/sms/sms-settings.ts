42797465136b0dea1ef701fd2a0f30d692d1bd13
holskil <andreasholskil@protonmail.com>
2019-12-05 12:45:46 +0100

    fix: removed a word in a sms message

--- src/sms/sms-settings.ts
+++ src/sms/sms-settings.ts
@@ -14,7 +14,7 @@ export const SMS_SETTINGS = {
     reminder: {
       'partly-payment': [
         'Hei - det nærmer seg frist for bøkene dine! Mange steder begynner å stenge før fristen 1.juli. Kun Boklistens ansatte kan ta imot bøker. Sjekk nettsiden for åpningstider og postadresse, samt informasjonsmailene vi har sendt ut. Mvh Boklisten.no',
-        'Hei! Du har bøker hvor andre avdrag straks forfaller. Kom innom for å selge bøkene tilbake til oss, eller betal andre avdrag på nettsidene våre. Se våre påminnelsesmailene våre for mer informasjon. Mvh Boklisten.no',
+        'Hei! Du har bøker hvor andre avdrag straks forfaller. Kom innom for å selge bøkene tilbake til oss, eller betal andre avdrag på nettsidene våre. Se påminnelsesmailene våre for mer informasjon. Mvh Boklisten.no',
         'Siste påminnelse! Selg bøkene dine tilbake til Boklisten.no for å slippe andre avdrag. Vi sender ut faktura på andre avdrag ved fristen. Se påminnelsesmailer for mer informasjon. Mvh Boklisten.no',
       ],
       rent: [
