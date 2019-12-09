e3aeb85691b17cf041c676d589ef4fbc76778262
holskil <andreasholskil@protonmail.com>
2019-12-09 14:48:33 +0100

    fix: added a sequence to sms settings for match

--- src/sms/sms-settings.ts
+++ src/sms/sms-settings.ts
@@ -8,7 +8,8 @@ export const SMS_SETTINGS = {
   text: {
     match: {
       none: [
-        'Melding til deg med kundeavtale hos Boklisten.no: Vennligst gi oss beskjed om hva som skal skje med bøkene dine: boklisten.no/match?deadline=201219',
+        'Melding til deg med kundeavtale hos Boklisten.no: Vennligst gi oss beskjed om hva som skal skje med bøkene dine: boklisten.no/m?d=201219',
+        'Hei fra oss i Boklisten.no. Minner om vår gjenbruksordning hvor du kan spare penger ved å levere til noen andre. Gi oss beskjed om hva som skal skje med bøkene dine: boklisten.no/m?d=201219',
       ],
     },
     reminder: {
