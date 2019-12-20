2b0a7c2866dde5f4ae8bf9feef4a44756cd193c1
holskil <andreasholskil@protonmail.com>
2019-12-20 15:57:25 +0100

    feat: reminder partly payment sequence 3

--- src/sms/sms-settings.ts
+++ src/sms/sms-settings.ts
@@ -18,6 +18,7 @@ export const SMS_SETTINGS = {
         'Hei - det nærmer seg frist for bøkene dine! Mange steder begynner å stenge før fristen 1.juli. Kun Boklistens ansatte kan ta imot bøker. Sjekk nettsiden for åpningstider og postadresse, samt informasjonsmailene vi har sendt ut. Mvh Boklisten.no',
         'Hei! Du har bøker hvor andre avdrag straks forfaller. Kom innom for å selge bøkene tilbake til oss, eller betal andre avdrag på nettsidene våre. Se påminnelsesmailene våre for mer informasjon. Mvh Boklisten.no',
         'Siste påminnelse! Selg bøkene dine tilbake til Boklisten.no for å slippe andre avdrag. Vi sender ut faktura på andre avdrag ved fristen. Se påminnelsesmailer for mer informasjon. Mvh Boklisten.no',
+        'Hei. Vi kan ikke se at du har betalt andre delbetaling av dine bøker. Vi har nå generert en faktura, men du skal få en siste mulighet til å betale. Absolutt siste frist er 31.12.2019. Se mail for mer info. Mvh Boklsiten.no',
       ],
       rent: [
         'Hei - det nærmer seg frist for bøkene dine! Mange steder begynner å stenge før fristen. Kun Boklistens ansatte kan ta imot bøker. Sjekk nettsiden for åpningstider og postadresse, samt informasjonsmailene vi har sendt ut. Mvh Boklisten.no',
